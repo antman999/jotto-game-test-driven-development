de93ebfa3154a55c54f1d589c8675fc057e77bf5
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-08 23:59:50 -0400

    Added test

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -5,3 +5,16 @@ import EnzymeAdapter from 'enzyme-adapter-react-16'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without error', () => {
+  
+})
+
+test('should render no text when the success prop is false ', () => {
+  
+})
+
+test('should render non empty congrats message when the success prop is true', () => {
+  
+})
+
