0bc7f27196189f5b40fbbe9fdf55271c378d7115
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-09 13:34:22 -0400

    added component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
-export default function Congrats() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function Congrats(props) {
+ 
+  if (props.success) {
+    return (
+      <div className="component-congrats">
+        <span className="congrats-message">
+          CONGRATS You guessed the word!
+          </span>
+      </div>
+    )
+  } else {
+    return (
+      <div className="component-congrats">
+
+      </div>
+    )
+  }
 }
