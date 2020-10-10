7c3ba90f3a06b0c55988177e4261d6bc97d35062
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-10-10 14:06:57 -0400

    filling in previous tests

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -18,6 +18,8 @@ test('should not throw warnings with expected props', () => {
 //describes are sections of where to test 
 describe('If there are no words guessed', () => {
   let wrapper 
+ //will run before each tests
+  //just abstracting the wrapper 
   beforeEach(() => {
     wrapper = setup({guessedWords:[]})
   })
@@ -37,6 +39,30 @@ describe('If there are no words guessed', () => {
 })
 
 describe('If there are words guessed', () => {
+  //we declare wrapper up here so it scopes 
+  let wrapper 
+  let guessedWords = [
+		{ guessedWord: 'train', letterMatchCount: 3 },
+		{ guessedWord: 'agile', letterMatchCount: 1 },
+		{ guessedWord: 'party', letterMatchCount: 5 },
+];
   
+  beforeEach(() => {
+    wrapper = setup({guessedWords})
+  })
+  test('should render without errors', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+
+  test('should render guessed words section', () => {
+    const guessedWordsDiv = findByTestAttr(wrapper, 'guessed-words')
+  })
+
+  test('should display the correct number of guessed words ', () => {
+    
+  })
+  
+ 
 })
 
